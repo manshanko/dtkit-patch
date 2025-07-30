2cd891fafc187bf24bc783cc64ce0292287d94d6
manshanko <manshanko@wartide.net>
2025-07-29 19:43:42 -0700

    remove error message helper function

--- src/main.zig
+++ src/main.zig
@@ -24,13 +24,31 @@ const MOD_PATCH_TAG = ".patch_999";
 const MOD_PATCH_STARTING_POINT_: u64 = 0xA33A4AA4AF26A69B;
 const MOD_PATCH_STARTING_POINT = std.mem.asBytes(&@byteSwap(MOD_PATCH_STARTING_POINT_));
 
+const already_patched_msg = "\"" ++ BUNDLE_DATABASE ++ "\" is already patched";
+
 pub fn main() u8 {
     const code: u8 = if (execute()) |msg| blk: {
         print(msg);
         break :blk 0;
     } else |err| blk: {
-        error_print(patch_error_msg(err));
-        if (os.console_will_close()) _ = os.display_message(patch_error_msg(err), .NotifyError);
+        const err_msg = err_msg: switch (err) {
+            error.AlreadyPatched => already_patched_msg,
+            error.UnsupportedDatabase => "found unsupported changes in \"" ++ BUNDLE_DATABASE ++ "\"",
+            error.BadFormat => "unknown format used in \"" ++ BUNDLE_DATABASE ++ "\"",
+            error.OutOfMemory => "out of memory",
+            error.NotFoundDatabase => "failed to find \"" ++ BUNDLE_DATABASE ++ "\"",
+            error.NotFoundBackup => "failed to find \"" ++ BUNDLE_DATABASE_BAK ++ "\"",
+            error.NotFoundDarktide => "failed to find Darktide installation directory",
+            error.BadPathName => "directory is an invalid path",
+            else => {
+                if (builtin.mode != .ReleaseSmall) {
+                    std.debug.print("{}\n", .{err});
+                }
+                break :err_msg "unexpected error";
+            }
+        };
+        error_print(err_msg);
+        if (os.console_will_close()) _ = os.display_message(err_msg, .NotifyError);
         break :blk 1;
     };
 
@@ -75,7 +93,7 @@ fn execute() ![]const u8 {
     if (options.patch) {
         const result = try apply_patch(allocator, db_path, db_bak_path);
         return switch (result) {
-            .AlreadyPatched => patch_error_msg(error.AlreadyPatched),
+            .AlreadyPatched => already_patched_msg,
             .AppliedPatch => "successfully patched \"" ++ BUNDLE_DATABASE ++ "\"",
         };
     } else if (options.unpatch) {
@@ -90,7 +108,7 @@ fn execute() ![]const u8 {
         // Default to toggle so running without arguments works (i.e. Explorer).
         const result = try toggle_patch(allocator, db_path, db_bak_path, interactive);
         return switch (result) {
-            .AlreadyPatched => patch_error_msg(error.AlreadyPatched),
+            .AlreadyPatched => already_patched_msg,
             .AppliedPatch => "successfully patched \"" ++ BUNDLE_DATABASE ++ "\"",
             .RemovedPatch => "successfully removed patch from \"" ++ BUNDLE_DATABASE ++ "\"",
         };
@@ -215,25 +233,6 @@ const PatchError = error{
     NotFoundBackup,
     NotFoundDarktide,
 };
-
-fn patch_error_msg(err: anyerror) [:0]const u8 {
-    return switch (err) {
-        error.AlreadyPatched => "\"" ++ BUNDLE_DATABASE ++ "\" is already patched",
-        error.UnsupportedDatabase => "found unsupported changes in \"" ++ BUNDLE_DATABASE ++ "\"",
-        error.BadFormat => "unknown format used in \"" ++ BUNDLE_DATABASE ++ "\"",
-        error.OutOfMemory => "out of memory",
-        error.NotFoundDatabase => "failed to find \"" ++ BUNDLE_DATABASE ++ "\"",
-        error.NotFoundBackup => "failed to find \"" ++ BUNDLE_DATABASE_BAK ++ "\"",
-        error.NotFoundDarktide => "failed to find Darktide installation directory",
-        error.BadPathName => "directory is an invalid path",
-        else => {
-            if (builtin.mode != .ReleaseSmall) {
-                std.debug.print("{}\n", .{err});
-            }
-            return "unexpected error";
-        }
-    };
-}
 
 fn io_stderr() std.fs.File {
     if (@hasDecl(std.fs.File, "stderr")) {
