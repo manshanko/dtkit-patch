fac23ca715b992e8c4b1bcf278a51007d4f820af
manshanko <manshanko@wartide.net>
2025-07-30 06:44:12 -0700

    use mem.indexOfPosLinear for targets other than ReleaseSmall

--- src/mem.zig
+++ src/mem.zig
@@ -6,6 +6,8 @@
 //
 // I do not know the relation between the extra contents .rdata and @memcpy/@memmove.
 // It looks to contain mostly f32. Zeroing it out didn't immediately crash when ran.
+const std = @import("std");
+
 const root = @import("root");
 
 pub fn memcpy(dest: anytype, src: anytype) void {
@@ -18,13 +20,17 @@ pub fn memcpy(dest: anytype, src: anytype) void {
 
 // based on std.mem.indexOfPosLinear
 pub inline fn index_of_pos(haystack: []const u8, start_index: usize, needle: []const u8) ?usize {
-    if (needle.len > haystack.len) return null;
+    if (root.disable_memcpy) {
+        if (needle.len > haystack.len) return null;
 
-    var i: usize = start_index;
-    const end = haystack.len - needle.len;
-    search: while (i <= end) : (i += 1) {
-        for (0..needle.len) |j| if (haystack[i + j] != needle[j]) continue :search;
-        return i;
+        var i: usize = start_index;
+        const end = haystack.len - needle.len;
+        search: while (i <= end) : (i += 1) {
+            for (0..needle.len) |j| if (haystack[i + j] != needle[j]) continue :search;
+            return i;
+        }
+        return null;
+    } else {
+        return std.mem.indexOfPosLinear(u8, haystack, start_index, needle);
     }
-    return null;
 }
