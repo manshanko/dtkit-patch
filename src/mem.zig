54384ba6077858615e08ac394f5c8f7a68c68ea3
manshanko <manshanko@wartide.net>
2025-11-25 17:57:12 -0800

    reorganize code from zig std library

--- src/mem.zig
+++ src/mem.zig
@@ -1,19 +1,6 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-// based on std.mem.indexOfPosLinear
-pub noinline fn index_of_pos(haystack: []const u8, start_index: usize, needle: []const u8) ?usize {
-    if (needle.len > haystack.len) return null;
-
-    var i: usize = start_index;
-    const end = haystack.len - needle.len;
-    search: while (i <= end) : (i += 1) {
-        for (0..needle.len) |j| if (haystack[i + j] != needle[j]) continue :search;
-        return i;
-    }
-    return null;
-}
-
 // When @memcpy/@memmove fail to inline on x86_64-windows-gnu it adds ~5KiB overhead.
 // If we export memcpy and memset symbols for ReleaseSafe/ReleaseSmall, even with the
 // same implementation, it doesn't happen.
@@ -30,10 +17,20 @@ comptime {
     }
 }
 
-// The memcpy_ and memset_ implementation is from compiler_rt in Zig.
-//
 // Copyright (c) Zig contributors
 // https://github.com/ziglang/zig/blob/d03a147ea0a590ca711b3db07106effc559b0fc6/LICENSE
+
+// zig/lib/std/mem.zig
+pub noinline fn index_of_pos(haystack: []const u8, start_index: usize, needle: []const u8) ?usize {
+    if (needle.len > haystack.len) return null;
+    var i: usize = start_index;
+    const end = haystack.len - needle.len;
+    search: while (i <= end) : (i += 1) {
+        for (0..needle.len) |j| if (haystack[i + j] != needle[j]) continue :search;
+        return i;
+    }
+    return null;
+}
 
 // zig/lib/compiler_rt/memcpy.zig
 fn memcpy_(noalias dest: ?[*]u8, noalias src: ?[*]u8, len: usize) callconv(.c) ?[*]u8 {
